ca67b4f2e8f4cd52959c549d517f593a3bb6f289
ltobywongl <tohongwong@gmail.com>
2024-07-22 02:58:23 +0800

    update schema

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,13 +1,27 @@
 import EnquiryForm from "@/components/enquiries/form";
 import { authOptions } from "@/utils/authOptions";
+import prisma from "@/utils/prisma";
 import { getServerSession } from "next-auth";
 
 async function Page() {
   const session = await getServerSession(authOptions);
 
+  const about = await prisma.websiteContent.findUnique({
+    select: {
+      content: true,
+    },
+    where: {
+      key: "about-us",
+    },
+  });
+
   return (
     <div className="py-2 flex justify-center">
-      <div className="flex flex-col gap-2 w-4/5">
+      <div className="flex flex-col gap-8 w-4/5">
+        <div>
+          <div className="text-lg font-bold">關於我們</div>
+          <div>{about?.content}</div>
+        </div>
         <div>
           <div className="text-lg font-bold">聯絡我們</div>
           <div>Email: abc@example.com</div>
@@ -15,7 +29,6 @@ async function Page() {
           <div>或在此提交查詢:</div>
           <EnquiryForm userId={session?.user.id} />
         </div>
-        <hr className="my-1" />
         <div>
           <div className="text-lg font-bold">條款及細則</div>
           <ol className="list-decimal">
--- app/order-history/page.tsx
+++ app/order-history/page.tsx
@@ -15,7 +15,7 @@ async function Page() {
       totalPrice: true,
       receiverName: true,
       receiverAddress: true,
-      approved: true,
+      state: true,
       createdAt: true,
       orderItem: {
         select: {
@@ -35,16 +35,17 @@ async function Page() {
 
   return (
     <div className="flex justify-center p-2">
-      <div className="w-4/5 flex flex-col gap-5">
+      <div className="w-full md:w-4/5 flex flex-col gap-5">
         {orders.map((order) => {
           return (
             <div className="p-3 border rounded-lg" key={order.id}>
-              <div className="font-bold">
-                下單日期 {order.createdAt.toLocaleDateString()}
+              <div className="font-bold flex justify-between">
+                <div>下單日期 {order.createdAt.toLocaleDateString()}</div>
+                <div>訂單狀態: {order.state}</div>
               </div>
+              <div>價格: ${order.totalPrice}</div>
               <div>
-                ${order.totalPrice}, 送往 {order.receiverName},{" "}
-                {order.receiverAddress}
+                送往 {order.receiverName}, {order.receiverAddress}
               </div>
               <hr className="my-1" />
               <div>
